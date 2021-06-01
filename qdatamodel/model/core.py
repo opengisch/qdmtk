2156901759321c004c35086d3e55af58356a6e1b
olivierdalang <olivier.dalang@gmail.com>
2021-06-01 16:34:15 +0200

    try to add alembic migrations
    unfortunately, not as nice as django. Geoalchemy not well
    supported, and no datamodel state in migrations (we could
    use reflection, but meh...).
    See README.md#notes

--- qdatamodel/model/core.py
+++ qdatamodel/model/core.py
@@ -3,12 +3,15 @@ import tempfile
 
 from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
 from sqlalchemy.event import listen
-from sqlalchemy.orm import column_property, declarative_base, sessionmaker
+from sqlalchemy.orm import column_property, declarative_base
 from sqlalchemy.sql.expression import cast
 
 from ..utils import NonExtendedGeometry
 
 Base = declarative_base()
+
+# sys.path.insert(0, r'C:\OSGeo4W\bin')
+# os.environ['PATH'] = r'C:\OSGeo4W\bin;' + os.environ['PATH']
 
 
 class Structure(Base):
@@ -37,31 +40,10 @@ engine = create_engine(f"sqlite:///{db_path}", echo=False)
 
 def load_spatialite(dbapi_conn, connection_record):
     dbapi_conn.enable_load_extension(True)
-    dbapi_conn.execute('SELECT load_extension("mod_spatialite")')
+    dbapi_conn.execute("SELECT load_extension('mod_spatialite')")
     dbapi_conn.execute("SELECT InitSpatialMetaData(1)")
 
 
 listen(engine, "connect", load_spatialite)
 
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-
-# Initial data
-session = Session()
-if session.query(Building).count() == 0:
-    session.add(
-        Building(
-            name="my house",
-            stories_count=4,
-            geom="SRID=4326;POINT(6.14 46.20)",
-        )
-    )
-    session.add(
-        Structure(
-            name="your house",
-            geom="SRID=4326;POINT(6.16 46.21)",
-        )
-    )
-    session.commit()
-session.close()
+# Base.metadata.create_all(engine)

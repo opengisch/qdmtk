7de093a4bc6dc49683fa883d69e20ef21d1cce13
olivierdalang <olivier.dalang@gmail.com>
2021-06-02 19:59:11 +0200

    simplify settings

--- qdatamodel/model/settings.py
+++ qdatamodel/model/settings.py
@@ -1,8 +1,6 @@
 import os
 import tempfile
 
-# DEBUG=True
-
 DATABASES = {
     "default": {
         "ENGINE": "django.contrib.gis.db.backends.spatialite",
@@ -13,11 +11,6 @@ DATABASES = {
 INSTALLED_APPS = [
     "qdatamodel.model.core",
 ]
-
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-SECRET_KEY = "REPLACE_ME"
 
 GDAL_LIBRARY_PATH = r"C:\OSGeo4W\bin\gdal302.dll"
 GEOS_LIBRARY_PATH = r"C:\OSGeo4W\bin\geos_c.dll"

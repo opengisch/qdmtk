19ae420df7cc54980fbd696dab0304dae4e0c4da
olivierdalang <olivier.dalang@gmail.com>
2021-07-07 16:53:25 +0200

    fix setup/deploy

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     author="Olivier Dalang",
     author_email="olivier@opengis.ch",
     url="https://github.com/opengisch/qdmtk",
-    packages=["qdmtk"],
     packages=find_packages(),
     install_requires=open("requirements.txt", encoding="utf-8").read().splitlines(),
     include_package_data=True,
